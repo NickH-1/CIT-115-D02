b08f8ce54904d0d0869c7ed8dd7495fa66cecf91
NickH-1 <nick.hassett1@gmail.com>
2024-10-03 09:45:18 -0400

    added comments

--- Inter Planetary Weights/Inter Planetary Weights.py
+++ Inter Planetary Weights/Inter Planetary Weights.py
@@ -1,6 +1,6 @@
 # Author: Nick Hassett
-#
 
+#Declare constants by fully capitalizing names and separating names with underscores for readability.
 MERCURY_SURFACE_GRAVITY_FACTOR = 0.38
 VENUS_SURFACE_GRAVITY_FACTOR = 0.91
 MOON_SURFACE_GRAVITY_FACTOR = 0.165
@@ -11,11 +11,13 @@ URANUS_SURFACE_GRAVITY_FACTOR = 0.92
 NEPTUNE_SURFACE_GRAVITY_FACTOR = 1.12
 PLUTO_SURFACE_GRAVITY_FACTOR = 0.066
 
-sName = input("Hello! What is your name?: ")
+sName = input("Hello! What is your name?: ") #Get user's name as string.
 
-userWeight = float(input("How much do you weigh?: "))
+userWeight = float(input("How much do you weigh?: ")) #Get user's weight and convert to float.
 
 print  (f"\n{sName}, here is your weight if you were on a different planet:")
+
+#Convert, format, and output user's weights on different planets.
 print  (f"Weight on Mercury:    {userWeight * MERCURY_SURFACE_GRAVITY_FACTOR:>10.2f}\n"
         f"Weight on Venus:      {userWeight * VENUS_SURFACE_GRAVITY_FACTOR:>10.2f}\n"
         f"Weight on our Moon:   {userWeight * MOON_SURFACE_GRAVITY_FACTOR:>10.2f}\n"
